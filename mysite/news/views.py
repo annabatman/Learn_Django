dad7cfacc1f7f9c8956d72e7e18da02a7c3e9dd3
Batman <annayermakova8@gmail.com>
2022-09-14 18:30:21 +0200

    Пройден 6 урок Добавлены контроллеры в приложение news

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    # print(request)
+    return HttpResponse('Hello world')
+
+
+def test(request):
+    return HttpResponse('<h1>Test page</h1>')
